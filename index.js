5b27170743967bbd801f9d50a25e1a65ae975f4d
Elliot Mangini <elliot.mangini@gmail.com>
2022-07-16 13:40:09 -0400

    commit tree 64e612554f0484ac4590abc0861c0bf457d3a375

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+
+
+const employee = { 
+    name: 'Johnathon', 
+    streetAddress: '1240 Summit Heights', 
+};
+
+
+
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = { ...employee }
+    updatedEmployee[key] = value;
+    return updatedEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const updatedEmployee = { ...employee }
+    delete updatedEmployee[key];
+    return updatedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
